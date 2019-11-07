2b1977db3acad2cfaa1217e57674d68ffa7ff98d
Joao Micena (JP) <joao.micena@nubank.com.br>
2019-11-07 18:48:07 -0300

    implement cat

--- src/com/rtjvm/scala/oop/files/File.scala
+++ src/com/rtjvm/scala/oop/files/File.scala
@@ -2,7 +2,7 @@ package com.rtjvm.scala.oop.files
 
 import com.rtjvm.scala.oop.filesystem.FilesystemException
 
-class File(override val parentPath: String, override val name: String, contents: String) extends DirEntry(parentPath, name) {
+class File(override val parentPath: String, override val name: String, val contents: String) extends DirEntry(parentPath, name) {
 
   def asDirectory: Directory =
     throw new FilesystemException("A file cannot be converted to a directory!")
